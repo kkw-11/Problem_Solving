a1d1b9d45f94a6e821afe289dd457033defa11a3
asus <kkw-11@naver.com>
2021-04-14 20:57:26 +0900

    Q59file update

--- C++를 이용한 창의적 문제 해결/59. 부분집합(MS 인터뷰 문제_DFS 완전탐색).cpp
+++ C++를 이용한 창의적 문제 해결/59. 부분집합(MS 인터뷰 문제_DFS 완전탐색).cpp
@@ -34,3 +34,40 @@ int main() {
 
 	return 0;
 }
+
+// //59. 부분집합(MS인터뷰:DFS)
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int n, ch[11];
+
+
+// void dfs(int level) {
+// 	if (level == n + 1) {
+// 		for (int i = 1; i <= n; ++i) {
+// 			if (ch[i] == 1)
+// 				printf("%d ", i);
+// 		}
+// 		printf("\n");
+
+// 		return;
+// 	}
+// 	else {
+// 		ch[level] = 1;
+// 		dfs(level + 1);
+
+// 		ch[level] = 0;
+// 		dfs(level + 1);
+// 	}
+// }
+
+// int main() {
+// 	freopen("input.txt", "rt", stdin);
+// 	scanf("%d", &n);
+
+// 	dfs(1);
+
+
+// 	return 0;
+// }
