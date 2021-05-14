6a8c20c66e83c6e8aa4aba7a51400fc533241eb6
asus <kkw-11@naver.com>
2021-05-14 15:11:14 +0900

    BOJ 이동하기

--- Study/인구 이동.cpp
+++ Study/인구 이동.cpp
@@ -5,8 +5,62 @@
 int drow[] = {-1,1,0,0};
 int dcol[] = {0,0,-1,1};
 int map[51][51], allycheck[51][51];
-int cnt, sum, allycount, N,L,R;
+int cnt, sum, allycount, N,L,R, r,c, sub,flag;
 
+void check(){
+
+    //i,j는 map의 현재 위치를 나타내는 변수
+    //k는 현재위치로부터 상하좌우 이동을 어디로 할지 정하는 변수
+    for(int i = 0;i<N;++i){
+        for(int j = 0; j<N;++j){
+
+
+            //i,j는 현재위치,r,c는 이웃 위치
+            for(int k = 0;k<4;++k){
+                r = i + drow[k];
+                c = j + dcol[k];
+                if(r>=0&&r<N && c>=0&&c<N){
+                     if((map[r][c] - map[i][j])>=L && (map[r][c] - map[i][j])<=R){
+                        if(allycheck[r][c] != 1){
+                            allycheck[r][c] = 1;
+                            sum += map[r][c];
+                            ++allycount;
+                        }
+                        if(allycheck[i][j] != 1){
+                            allycheck[i][j] = 1;
+                            sum += map[i][j];
+                            ++allycount;
+                        }
+
+                     }
+                }
+
+            }
+        }
+    }
+}
+void renewal(){
+    for(int i = 0;i<N;++i){
+        for(int j = 0;j<N;++j){
+            if(allycheck[i][j] == 1){
+                map[i][j] = sum/allycount;
+                flag = 1;
+            }
+
+        }
+    }
+    if(flag){
+        ++cnt;
+    }
+}
+void reset(){
+    flag = 0;
+    for(int i = 0; i<N;++i){
+        for(int j = 0 ; j<N;++j){
+            allycheck[i][j] = 0;
+        }
+    }
+}
 int main(){
     freopen("input.txt","rt",stdin);
     //입력값 받기
@@ -17,12 +71,21 @@ int main(){
             scanf("%d",&map[i][j]);
         }
     }
-    //연합국 인지 확인(연합국 allycheck 배열 0->1, 연합국 아니면(상하좌우 차가 모두 문제의 주어진 범위에 속하지 않으면) -1)
+    while(1){
+        //연합국 인지 확인(연합국 allycheck 배열 0->1, 연합국 아니면(상하좌우 차가 모두 문제의 주어진 범위에 속하지 않으면) -1)
+        check();
+        //연합국의 각 인구 계산
 
-    //연합국의 각 인구 계산
-
-    //연합국 인구 갱신
-
-    //위 과정 반복
+        //연합국 인구 갱신
+        renewal();
+        
+        if(flag !=1)
+            break;
+        //위 과정 반복
+        reset();
+    }
+    
+    printf("%d",cnt);
+    
     return 0;
 }
