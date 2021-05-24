34f93ea931e566f373acec8d2c587df7fb9fc6c1
asus <kkw-11@naver.com>
2021-05-24 13:35:37 +0900

    아기상어

--- Study2/아기상어_0523.cpp
+++ Study2/아기상어_0523.cpp
@@ -1,107 +1,221 @@
-//https://www.acmicpc.net/problem/16236
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-#include<algorithm>
+// //https://www.acmicpc.net/problem/16236
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-#define MAX 21
-#define MIN 987654321
+int map[20][20];
+int visited[20][20];
+int dr[] = { -1,1,0,0 };
+int dc[] = { 0,0,-1,1 };
+int n;
+int Sharksize = 2;
+int ateCnt;
+struct Shark {
+	int row, column;
+};
+vector<pair<int, pair<int, int>>> eatFish;
 
-int N;
-int result = 0;
-int eat_count = 0;
-int baby_size = 2;
-int eat_distance;
-int arr[MAX][MAX];
-int visit[MAX][MAX];
-int Baby_x, Baby_y;
-vector <pair <pair<int, int>, int>> Eat; //<<거리,x>y>
+void BFS(Shark sharkStart) {
+	
 
-int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { -1, 1, 0, 0 };
+	Shark posi;
+	posi.row = sharkStart.row;
+	posi.column = sharkStart.column;
 
-void BFS(int init_x, int init_y) {
-	eat_distance = MIN;
-	Eat.clear();
-	memset(visit, 0, sizeof(visit));
+	queue<Shark> q;
+	q.push(posi);
 
-	queue<pair<int, int>> que; 
-	que.push(make_pair(init_x, init_y));
+	while (!q.empty()) {
+		Shark cur;
+		Shark next;
 
-	while (!que.empty()) {
-		int x = que.front().first; 
-		int y = que.front().second;
-		que.pop();
+		cur.row = q.front().row;
+		cur.column = q.front().column;
+		q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i]; 
-			int ny = y + dy[i];
-			
-			if (0 > nx || N <= nx || 0 > ny || N <= ny) continue;
+		for (int i = 0; i < 4; ++i) {
+			next.row = cur.row + dr[i];
+			next.column = cur.column + dc[i];
 
-			if (visit[nx][ny] == 0 && baby_size >= arr[nx][ny]) { // 자기보다 큰 물고기 자리 못감
-				visit[nx][ny] = visit[x][y] + 1;
-				if (arr[nx][ny] > 0 && arr[nx][ny] < baby_size) {// 자기보다 작은 물고기 꿀꺽
-					if (eat_distance >= visit[nx][ny]) {
-						eat_distance = visit[nx][ny];
-						Eat.push_back(make_pair(make_pair(eat_distance, nx), ny));
-					}
+			if (next.row < 0 || next.row >= n || next.column < 0 || next.column >= n)continue;
+			if (visited[next.row][next.column]) continue;
+			if (map[next.row][next.column] == 0 || map[next.row][next.column] <= Sharksize) {
+				visited[next.row][next.column] = visited[cur.row][cur.column] + 1;
+
+				q.push(next);
+
+				if (map[next.row][next.column] > 0 && map[next.row][next.column] < Sharksize) {
+					eatFish.push_back({ visited[next.row][next.column], { next.row, next.column} });
 				}
-				que.push(make_pair(nx, ny));
+
 			}
+
 		}
 	}
-
-}
-
-void init() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
 }
 
 int main() {
-	init();
-	cin >> N;
+//	freopen("input.txt", "rt", stdin);
+	Shark sharkStart;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> arr[i][j];
+	scanf("%d", &n);
 
-			if (arr[i][j] == 9) { //아기 상어
-				arr[i][j] = 0;
-				Baby_x = i; 
-				Baby_y = j;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			scanf("%d", &map[i][j]);
+			if (map[i][j] == 9) {
+				map[i][j] = 0;
+				sharkStart.row = i;
+				sharkStart.column = j;
 			}
 		}
 	}
-	
-	while (1) {
-		BFS(Baby_x, Baby_y);
 
-		if (Eat.empty()) break;
+	int flag = true;
+	int result = 0;
+
+	while (flag) {
+		flag = false;
+
+		BFS(sharkStart);
+
+		if (eatFish.size() == 0) break;
 		else {
-			sort(Eat.begin(), Eat.end());//
-			eat_count++;
-			result += Eat[0].first.first;//(distance,x,y)
-			arr[Eat[0].first.second][Eat[0].second] = 0;
-			//물고기 먹은후 상어 위치 갱신
-			Baby_x = Eat[0].first.second; 
-			Baby_y = Eat[0].second;
-			if (baby_size == eat_count) {
-				baby_size++; 
-				eat_count = 0;
+			flag = true;
+			sort(eatFish.begin(), eatFish.end());
+			result += eatFish[0].first;
+			sharkStart.row = eatFish[0].second.first;
+			sharkStart.column = eatFish[0].second.second;
+			map[eatFish[0].second.first][eatFish[0].second.second] = 0;
+			++ateCnt;
+			if (ateCnt == Sharksize) {
+				++Sharksize;
+				ateCnt = 0;
 			}
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < n; ++j) {
+					visited[i][j] = 0;
+				}
+			}
+
+			eatFish.clear();
+			
 		}
 	}
 
-	cout << result;
+	printf("%d", result);
+
 	return 0;
 }
+
+
+// #include<iostream>
+// #include<queue>
+// #include<vector>
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// #include<math.h>
+// #include<algorithm>
+// using namespace std;
+
+// #define MAX 21
+// #define MIN 987654321
+
+// int N;
+// int result = 0;
+// int eat_count = 0;
+// int baby_size = 2;
+// int eat_distance;
+// int arr[MAX][MAX];
+// int visit[MAX][MAX];
+// int Baby_x, Baby_y;
+// vector <pair <pair<int, int>, int>> Eat; //< <거리,x> y>
+
+// int dx[4] = { 0, 0, -1, 1 };
+// int dy[4] = { -1, 1, 0, 0 };
+
+// void BFS(int init_x, int init_y) {
+// 	eat_distance = MIN;
+// 	Eat.clear();
+// 	memset(visit, 0, sizeof(visit));
+
+// 	queue<pair<int, int> > que; 
+
+// 	que.push(make_pair(init_x, init_y));
+
+// 	while (!que.empty()) {
+// 		int x = que.front().first; 
+// 		int y = que.front().second;
+// 		que.pop();
+
+// 		for (int i = 0; i < 4; i++) {
+// 			int nx = x + dx[i]; 
+// 			int ny = y + dy[i];
+			
+// 			if (0 > nx || N <= nx || 0 > ny || N <= ny) continue;
+
+// 			if (visit[nx][ny] == 0 && baby_size >= arr[nx][ny]) { // 자기보다 큰 물고기 자리 못감
+// 				visit[nx][ny] = visit[x][y] + 1;
+
+// 				if (arr[nx][ny] > 0 && arr[nx][ny] < baby_size) {// 자기보다 작은 물고기 꿀꺽
+					
+						// Eat.push_back(make_pair(make_pair(eat_distance, nx), ny));
+// 				}
+
+// 				que.push(make_pair(nx, ny));
+// 			}
+// 		}
+// 	}
+
+// }
+
+// void init() {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(0); cout.tie(0);
+// }
+
+// int main() {
+// 	init();
+// 	cin >> N;
+
+// 	for (int i = 0; i < N; i++) {
+// 		for (int j = 0; j < N; j++) {
+// 			cin >> arr[i][j];
+
+// 			if (arr[i][j] == 9) { //아기 상어
+// 				arr[i][j] = 0;
+// 				Baby_x = i; 
+// 				Baby_y = j;
+// 			}
+// 		}
+// 	}
+	
+// 	while (1) {
+// 		BFS(Baby_x, Baby_y);
+
+// 		if (Eat.empty()) break;
+// 		else {
+// 			sort(Eat.begin(), Eat.end());//
+// 			eat_count++;
+// 			result += Eat[0].first.first;//(distance,x,y) //< <거리,x> y>
+// 			arr[Eat[0].first.second][Eat[0].second] = 0;
+// 			//물고기 먹은후 상어 위치 갱신
+// 			Baby_x = Eat[0].first.second; 
+// 			Baby_y = Eat[0].second;
+			
+// 			if (baby_size == eat_count) {
+// 				baby_size++; 
+// 				eat_count = 0;
+// 			}
+// 		}
+// 	}
+
+// 	cout << result;
+// 	return 0;
+// }
 
 // //https://www.acmicpc.net/problem/16236
 // //변수명 수정
