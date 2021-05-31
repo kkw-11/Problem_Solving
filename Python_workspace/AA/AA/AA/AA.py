4aa7998d21a54d51ae76356ec4f9b510ef99baeb
asus <kkw-11@naver.com>
2021-05-31 16:36:48 +0900

    에라토스테네스체

--- Python_workspace/AA/AA/AA/AA.py
+++ Python_workspace/AA/AA/AA/AA.py
@@ -1,29 +1,41 @@
+#import sys
+##sys.stdin = open("input.txt","rt")
+
+#n = int(input())
+#cnt = 0
+#ch = [0]*(n+1)
+
+
+#for i in range(2,n+1):
+#    if ch[i] == 0:
+#        cnt += 1
+#        for j in range(i,n+1,+i):
+#            ch[j] = 1
+
+#print(cnt)
+        
+
+
+
+
+
 import sys
 #sys.stdin = open("input.txt","rt")
 
+cnt = 0
+def isPrime(x):
+    if(x == 2 ):
+        return 1
+    temp =int(x/2)
+    for i in range(2,temp+1):
+        if x % i == 0:
+            return 1
+    return 0
+
 n = int(input())
-num = list(map(int,input().split()))
+for i in range(2,n+1):
+    flag = isPrime(i)
+    if flag != 1:
+        cnt += 1
 
-#def digit_sum(x):
-#    digitSum = 0
-#    while x:
-#        digitSum += x %10
-#        x //= 10
-#    return digitSum
-
-def digit_sum(x):
-    digitSum = 0
-    for i in str(x):
-        digitSum += int(i)
-
-    return digitSum
-
-max = -1
-
-for i in range(n):
-    total = digit_sum(num[i])
-    if total > max:
-        max = total
-        answer = num[i]
-
-print(answer)
+print(cnt+1)
