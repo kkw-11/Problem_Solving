b48b86e130904fd0e25c71fbdb6e481ff22f8b9e
asus <kkw-11@naver.com>
2021-06-08 09:42:27 +0900

    숫자만 추출 추출.cpp code refactoring

--- C++를 이용한 창의적 문제 해결/06. 숫자만 추출.cpp
+++ C++를 이용한 창의적 문제 해결/06. 숫자만 추출.cpp
@@ -1,33 +1,91 @@
 //6.숫자만 추출
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
 	//freopen("input.txt", "rt", stdin);
-	char ch[10];
-	int num = 0;
-	int ten = 0;
+	char str[51];
+	scanf("%s", str);
+	int res = 0;
 	int cnt = 0;
-	scanf("%s", ch);
-	for (int i = 0; ch[i]; ++i) {
-		if (ch[i] >= 48 && ch[i] <= 57) {
-			num = ((num * 10) + (ch[i] - 48));
-			++ten;
+	for (int i = 0; str[i]; ++i) {
+		if (str[i] >= 48 && str[i] <= 57) {
+			res = res * 10 + (str[i] - 48);
 		}
 	}
 
-	int j = num;
-	for (int i = 1; i < j; ++i) {
-		if (num % i == 0) {
-			j = num / i;
-			cnt += 2;;
+	for (int i = 1; i * i <= res; ++i) {
+		if (i * i == res) {
+			++cnt;
+			break;
+		}
+		if (res % i == 0) {
+			cnt += 2;
 		}
 	}
-	printf("%d\n", num);
-	printf("%d", cnt);
 
 
+	printf("%d\n%d", res, cnt);
 	return 0;
 }
+
+
+////효율성 개선전 코드
+// //6.숫자만 추출
+// #include <stdio.h>
+
+// int main() {
+// 	//freopen("input.txt", "rt", stdin);
+// 	char str[51];
+// 	scanf("%s", str);
+// 	int res = 0;
+// 	int cnt = 0;
+// 	for (int i = 0; str[i]; ++i) {
+// 		if (str[i] >= 48 && str[i] <= 57) {
+// 			res = res * 10 + (str[i] - 48);
+// 		}
+// 	}
+
+// 	for (int i = 1; i <= res; ++i) {
+// 		if (res % i == 0) {
+
+// 			++cnt;
+// 		}
+// 	}
+
+// 	printf("%d\n%d", res, cnt);
+// 	return 0;
+// }
+
+// //6.숫자만 추출
+// #include<stdio.h>
+
+// int main() {
+// 	//freopen("input.txt", "rt", stdin);
+// 	char ch[10];
+// 	int num = 0;
+// 	int ten = 0;
+// 	int cnt = 0;
+// 	scanf("%s", ch);
+// 	for (int i = 0; ch[i]; ++i) {
+// 		if (ch[i] >= 48 && ch[i] <= 57) {
+// 			num = ((num * 10) + (ch[i] - 48));
+// 			++ten;
+// 		}
+// 	}
+
+// 	int j = num;
+// 	for (int i = 1; i < j; ++i) {
+// 		if (num % i == 0) {
+// 			j = num / i;
+// 			cnt += 2;;
+// 		}
+// 	}
+// 	printf("%d\n", num);
+// 	printf("%d", cnt);
+
+
+// 	return 0;
+// }
 
 // //6.숫자만 추출
 // #include<stdio.h>
