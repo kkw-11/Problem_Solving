502482620a2569eb56fc9ec4b13e54554af20c34
asus <kkw-11@naver.com>
2021-06-13 09:31:28 +0900

    프로그래머스 순위.py

--- Study2/순위_0613.py
+++ Study2/순위_0613.py
@@ -4,9 +4,9 @@
 #sys.stdin = open("input.txt","rt")
 #인접리스트 방식으로 순위 표현
 '''
-idea는 각 플레리어의 경기 기록이 이미(n-1)이거나 현재 기록된 승패여부로 추측을 통해 
-승,패 알수 있는 선수의 수가 (n-1)개수이면 순위를 만들 수 있음
- 5가 2한테 지면 5는 2를 이기는 애들한테 모두 진다. 이 방식으로 현재 기록으로 나머지 선수들과의 승패여부 확인
+idea는 각 플레리어의 경기 기록이 이미(n-1)이거나 현재 기록된 승패여부로 문제의 주어진 조건에 맞게 탐색을 통해 
+승,패 알 수 있는 선수의 수가 (n-1)개수이면 순위를 만들 수 있음
+ ex) 5가 2한테 지면 5는 2를 이기는 애들한테 모두 진다. 이 방식으로 현재 기록으로 나머지 선수들과의 승패여부 탐색 확인
  이렇게 확인 된 수가 n-1이 되면 됨
 '''
 cnt = 0
@@ -49,21 +49,26 @@ def solution(n, results):
     # print()
     #        [ 0    1         2                                   3                  4                5     ] 
     #graph = [[], [(2, 1)], [(4, -1), (3, -1), (1, -1), (5, 1)], [(4, -1), (2, 1)], [(3, 1), (2, 1)], [(2, -1)]]
-    for i in range(1,n+1):
+    for player in range(1,n+1):
+        if len(graph[player]) == n-1:
+            answer +=1
+            continue
+
         for j in range(1,n+1):
             visited[j]= False
         cnt = 0
-        if len(graph[i]) == n-1:
-            answer +=1
-            continue
-        # 승 수 계산
 
-        DFS(n, graph,visited,i,1)
-        
-        for j in range(1,n+1):
-            visited[j]= False
+        # 승 수 계산
+        DFS(n, graph,visited,player,1)
+        '''
+        # 한 player가 다른 player에게 이기면서 지는 경우는 없기 때문 (player 2: (4,1) (4,-1) 동시에 있는 경우는 없기 때문) 현재 player Vertex에서 
+        # 시작할 때 승수 체크전 한번만 초기화 하면 됨 방문한 곳을 다시 방문할 일이 없음
+        '''
+        # for j in range(1,n+1): 
+        #     visited[j]= False  
+
         # 패 수 계산
-        DFS(n,graph,visited,i,-1)
+        DFS(n,graph,visited,player,-1)
 
         if cnt == n-1:
             answer += 1
