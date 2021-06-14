f20200af428a756587b17139cc4b655c99335229
asus <kkw-11@naver.com>
2021-06-14 12:13:54 +0900

    여행경로

--- Python_workspace/AA/AA/AA/AA.py
+++ Python_workspace/AA/AA/AA/AA.py
@@ -1,26 +1,42 @@
 from collections import defaultdict
 answer  = []
-last = ""
-# 스택을 사용한 DFS
-def dfs(routes, start):
+cnt = 0
+# tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"], ["ICN","AAA"],["AAA","BBB"],["IAD","ICN"]]
 
-    global answer
-    answer.append(start)
-    # ["ICN":["SFO","ATL"]]
-    for idx, toValue in enumerate(routes[start]):
-        if routes[start][idx] in routes:  #도착점이 routes에 key로 있어야 그 곳에서 다시 출발할 수 있음
-            dfs(routes,toValue)
+'''
+ICN:[(AAA,0),(JFK,0)] 
+JFK:[(HND,0)]
+HND:[(IAD,0)]
+IAD:[(ICN,0)]
+AAA:[(BBB)]
+'''
+# 스택을 사용한 DFS
+def dfs(routes, start, ticketSize,moveCnt):
+    if moveCnt == ticketSize or start not in routes:
+
+        return answer
+    elif moveCnt <ticketSize:
+        for toValueInfo in routes[start]:
+            if toValueInfo[1]: continue # 방문 했으면 다음 티켓 정보 확인
+            if toValueInfo[0] in routes:
+            moveCnt += 1
+            dfs(routes, toValueInfo[0],ticketSize,moveCnt)
+
+
+
 
 def solution(tickets):
-    # 해쉬 딕셔너리로 특정 티켓의 인접 리스트로 그래프 표현
+    ticketSize = len(tickets)
+    # 해쉬 딕셔너리로 tickets 정보 인접 리스트 그래프로 표현
     routes = defaultdict(list)
     for fromKey, toValue in tickets: 
-        routes[fromKey].append(toValue)
+        routes[fromKey].append((toValue,0))
         
     for rKey in routes:
         routes[rKey].sort()
-
-    dfs(routes,"ICN")
+    
+    answer.append("ICN")
+    dfs(routes,"ICN",ticketSize,0)
     answer.extend(routes[answer[-1]])
     
     return answer
