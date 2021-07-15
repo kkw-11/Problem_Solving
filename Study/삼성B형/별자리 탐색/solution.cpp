22b7cfbb63c3146deef78215dfb422cd6b5ae5bc
asus <kkw-11@naver.com>
2021-07-15 19:56:00 +0900

    210715

---
+++ Study/삼성B형/별자리 탐색/solution.cpp
@@ -0,0 +1,19 @@
+#define MAX_N 1000
+#define MAX_M 20
+
+struct Result {
+	int y, x;
+};
+
+void init(int N, int M, int Map[MAX_N][MAX_N])
+{
+
+}
+
+Result findConstellation(int stars[MAX_M][MAX_M])
+{
+	Result res;
+
+	res.y = res.x = 0;
+	return res;
+}
---
+++ Study/삼성B형/테이블계산기/solution.cpp
@@ -0,0 +1,12 @@
+#define MAXR		99
+#define MAXC		26
+
+void init(int C, int R) {
+	
+}
+void set(int col, int row, char input[]) {
+	
+}
+void update(int value[MAXR][MAXC]) {
+	
+}
