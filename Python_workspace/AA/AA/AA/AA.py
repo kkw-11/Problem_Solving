9961c0bfb85013c8819e0e3bb72eec9d0d8e0a08
asus <kkw-11@naver.com>
2021-07-27 15:09:44 +0900

    210727 BOJ 1152

--- Python_workspace/AA/AA/AA/AA.py
+++ Python_workspace/AA/AA/AA/AA.py
@@ -1,28 +1,9 @@
 import sys
 
-input_data = sys.stdin.readline().rstrip()
-cnt = [0] * 26
-maxCnt = 0
-idx = list()
+input_data = sys.stdin.readline().rstrip().split()
 
-for elem in input_data:
-    ascii = ord(elem)
+print(len(input_data))
 
-    if ascii >= 65 and ascii <= 90:
-        cnt[ascii - 65] += 1
-    elif ascii >= 97 and ascii <=122:
-        cnt[ascii - 97] += 1
-
-maxCnt = max(cnt)
-
-for i in range(len(cnt)):
-    if maxCnt == cnt[i]:
-        idx.append(i)
-
-if len(idx) == 1:
-    print(chr(idx[0]+65))
-elif len(idx) > 1:
-    print("?")
 
 #import sys
 #import heapq
