a43eee4aa182ccff2368a633d8c297adad66eb7e
asus <kkw-11@naver.com>
2021-07-27 14:09:07 +0900

    210727 BOJ 10809

--- Python_workspace/AA/AA/AA/AA.py
+++ Python_workspace/AA/AA/AA/AA.py
@@ -1,42 +1,57 @@
 import sys
-import heapq
-sys.stdin = open("input.txt","rt")
 
-n, m = map(int,sys.stdin.readline().rstrip().split())
+input_data = sys.stdin.readline().rstrip()
 
-cakes = list(map(int, sys.stdin.readline().rstrip().split()))
+answer = [-1]*26
 
-print(n,m)
-print(cakes)
+for i in range(len(input_data)):
+    if answer[ord(input_data[i])-97] == -1 :
+        answer[ord(input_data[i])-97] = i
 
-tenMulti = []
-notTenMulti = []
-answer = 0
-curCutCnt = 0
-totalCutCnt = 0
+for elem in answer:
+    print(elem,end=" ")
 
-for cake in cakes:
-    if cake == 10:
-        answer += 1
-    elif cake % 10 == 0 :
-        heapq.heappush(tenMulti,cake)
-    else:
-        heapq.heappush(notTenMulti,cake)
 
-print(tenMulti)
-print(notTenMulti)
 
-while tenMulti:
-    cake = heapq.heappop(tenMulti)
+#import sys
+#import heapq
+#sys.stdin = open("input.txt","rt")
 
-    curCutCnt = cake // 10
+#n, m = map(int,sys.stdin.readline().rstrip().split())
 
-    if curCutCnt > m - totalCutCnt:
-        anser += m - totalCutCnt
-        break
-    elif curCutCnt == m - totalCutCnt:
-        answer += (curCutCnt + 1)
-        break
-    else:
-        answer += (curCutCnt + 1)
-        totalCutCnt += curCutCnt
+#cakes = list(map(int, sys.stdin.readline().rstrip().split()))
+
+#print(n,m)
+#print(cakes)
+
+#tenMulti = []
+#notTenMulti = []
+#answer = 0
+#curCutCnt = 0
+#totalCutCnt = 0
+
+#for cake in cakes:
+#    if cake == 10:
+#        answer += 1
+#    elif cake % 10 == 0 :
+#        heapq.heappush(tenMulti,cake)
+#    else:
+#        heapq.heappush(notTenMulti,cake)
+
+#print(tenMulti)
+#print(notTenMulti)
+
+#while tenMulti:
+#    cake = heapq.heappop(tenMulti)
+
+#    curCutCnt = cake // 10
+
+#    if curCutCnt > m - totalCutCnt:
+#        anser += m - totalCutCnt
+#        break
+#    elif curCutCnt == m - totalCutCnt:
+#        answer += (curCutCnt + 1)
+#        break
+#    else:
+#        answer += (curCutCnt + 1)
+#        totalCutCnt += curCutCnt
