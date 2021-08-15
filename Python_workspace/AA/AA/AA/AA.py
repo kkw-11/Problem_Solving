24bcdb12b640e90da7daa71095e60755e7497333
asus <kkw-11@naver.com>
2021-08-15 23:53:09 +0900

    210815 BOJ 11725

--- Python_workspace/AA/AA/AA/AA.py
+++ Python_workspace/AA/AA/AA/AA.py
@@ -1,36 +1,83 @@
 import sys
+from collections import deque
 
-res = set()
-nums = []
+sys.stdin = open("input.txt")
 
-def subSet(depth, sum, n, nums):
+def go(start, graph, visited):
+    global res
+    q = deque()
+    q.append(start)
 
-    if depth == n:
-        res.add(sum)
-        return 
 
-    sum += nums[depth]
-    subSet(depth+1,sum, n, nums)
-    
-    sum -= nums[depth]
-    subSet(depth+1,sum, n, nums)
+    while q:
+        parent = q.popleft()
+        visited[parent] = True
 
-#sys.stdin = open("input.txt")
-    
+
+        for child in graph[parent]:
+            if not visited[child]:
+                res[child] = parent
+                q.append(child)
+
+
+
 n = int(sys.stdin.readline().rstrip())
 
-nums = list(map(int,sys.stdin.readline().rstrip().split()))
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+res = [None]*(n+1)
 
-subSet(0,0,n,nums)
 
-res = list(res)
-res.sort()
-for i in range(len(res)):
-    if res[i] != i:
-        print(i)
-        break
-else:
-    print(res[-1]+1)
+for _ in range(n-1):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+go(1,graph,visited)
+
+for i in range(2,n+1):
+    print(res[i])
+
+
+
+#import sys
+
+#sys.stdin = open("input.txt")
+
+#def go(parent, graph, visited):
+#    global res
+#    visited[parent] = True
+
+#    for child in graph[parent]:
+#        if not visited[child]:
+#            res[child] = parent
+#            go(child, graph,visited)
+
+
+#n = int(sys.stdin.readline().rstrip())
+
+#graph = [[] for _ in range(n+1)]
+#visited = [False]*(n+1)
+#res = [None]*(n+1)
+
+
+#for _ in range(n-1):
+#    a, b = map(int, sys.stdin.readline().rstrip().split())
+#    graph[a].append(b)
+#    graph[b].append(a)
+
+#go(1,graph,visited)
+
+#for i in range(2,n+1):
+#    print(res[i])
+
+
+
+
+
+
+
+
 
 
 #import sys
