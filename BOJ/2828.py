cc12a1a52009c26de7adc1f8847b028b5bb18f68
asus <kkw-11@naver.com>
2021-11-09 11:41:52 +0900

    211109 BOJ 2828

--- BOJ/2828.py
+++ BOJ/2828.py
@@ -1,31 +1,29 @@
 import sys
 
 input = sys.stdin.readline
+leftmost = 1
+
 screen, basket_size = map(int,input().split())
 
 number_of_apples = int(input())
-position_info = []
-
-for _ in range(number_of_apples):
-    position_info.append(int(input()))
-basket_left = 1
-basket_right = basket_left + basket_size
-
+basket_left = leftmost
+basket_right = leftmost + basket_size - 1
 move = 0
 
-for apple_position in position_info:
-    if apple_position >= basket_left and apple_position < basket_right:
+for _ in range(number_of_apples):
+    apple_position = int(input())
+
+    if basket_left <= apple_position <= basket_right: 
         continue
-    else: #apple_position < basket_left or apple_position >= basket_right
+    else: 
         if apple_position < basket_left:
             cur_move = basket_left - apple_position
             basket_left = basket_left - cur_move
             basket_right = basket_right - cur_move
             move += cur_move
-        elif apple_position >= basket_right:
-            cur_move = abs(apple_position + 1 - basket_right)
+        elif apple_position > basket_right:
+            cur_move = apple_position - basket_right
             basket_left = basket_left + cur_move
             basket_right = basket_right + cur_move 
             move += cur_move
-
 print(move)
