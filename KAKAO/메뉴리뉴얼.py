c92dc0623d50e930b6303b55b7d5754696ed5cc6
asus <kkw-11@naver.com>
2021-12-30 01:23:51 +0900

    211230 refactor: 메뉴 리뉴얼.py

--- KAKAO/메뉴리뉴얼.py
+++ KAKAO/메뉴리뉴얼.py
@@ -7,37 +7,26 @@ def solution(orders, course):
         course_menu_dict = {}
         
         for order in orders:
-            all_course = list(combinations(sorted(order),size_of_course))
-
+            all_course = list(map("".join,list(combinations(sorted(order),size_of_course))))
+        
             for each_course in all_course:
-                each_course_str = "".join(each_course)
-                if not each_course_str in course_menu_dict:
-                    course_menu_dict[each_course_str] = 1
+                if not each_course in course_menu_dict:
+                    course_menu_dict[each_course] = 1
                 else:
-                    course_menu_dict[each_course_str] += 1
+                    course_menu_dict[each_course] += 1
 
         else:
-            max_cnt = 0
-            course_menu = None
-            cnt_dict = {}
-            for c in course_menu_dict:
-                if course_menu_dict[c] >= 2:
-                    if not course_menu_dict[c] in cnt_dict:
-                        cnt_dict[course_menu_dict[c]] = [c]
-                    else:
-                        cnt_dict[course_menu_dict[c]].append(c)
-                    if max_cnt < course_menu_dict[c]:
-                        max_cnt = course_menu_dict[c]
-            else:
-                if max_cnt != 0:
-                    for cc in cnt_dict[max_cnt]:
-                        result.append(cc)
+            if len(course_menu_dict) == 0:
+                continue
+            max_val = max(course_menu_dict.values())
+            if max_val >= 2:
+                for key, value in course_menu_dict.items():
+                    if value == max_val:
+                        result.append(key)
 
     result.sort()
-            
 
     return result
-
 
 # from itertools import combinations
 
