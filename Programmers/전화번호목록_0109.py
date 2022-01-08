5c0eafc339bea56782b0d4110a56e7bfaae18c03
asus <kkw-11@naver.com>
2022-01-09 00:28:59 +0900

    220109 Programmers 전화번호 목록

---
+++ Programmers/전화번호목록_0109.py
@@ -0,0 +1,20 @@
+def solution(phone_book):
+    n = len(phone_book)
+    check_dict = {}
+
+    for phone_number in phone_book:
+        if not phone_number in check_dict:
+            check_dict[phone_number] = True
+
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+
+            if len(temp) != len(phone_number):
+                if not temp in check_dict:
+                    pass
+                elif check_dict[temp]:
+                    return False
+    else:
+        return True
