e989e665421be95e8d349fe15038d55aea107375
asus <kkw-11@naver.com>
2022-01-10 20:46:54 +0900

    220110 Programmers 전화번호목록

--- Programmers/전화번호목록_0109.py
+++ Programmers/전화번호목록_0109.py
@@ -3,14 +3,18 @@ def solution(phone_book):
     check_dict = {}
 
     for phone_number in phone_book:
-        check_dict[phone_number] = True
+        if not phone_number in check_dict:
+            check_dict[phone_number] = True
 
     for phone_number in phone_book:
         temp = ""
         for number in phone_number:
             temp += number
 
-            if temp != phone_number and temp in check_dict:
-                return False
+            if len(temp) != len(phone_number):
+                if not temp in check_dict:
+                    pass
+                elif check_dict[temp]:
+                    return False
     else:
-        return True
+        return Truet
