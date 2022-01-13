a5cd6b32d391ac3f06ce1e8acabb677b3c7f1ee1
asus <kkw-11@naver.com>
2022-01-13 13:28:55 +0900

    210113 전화번호 목록.py

--- Programmers/전화번호목록_0110.py
+++ Programmers/전화번호목록_0110.py
@@ -1,13 +1,16 @@
 def solution(phone_book):
     check_dict = {}
 
-    for phone_number in phone_book:
-        check_dict[phone_number] = True
 
+    for phone_number in phone_book: 
+        check_dict[phone_number] = True
+ 
+ 
     for phone_number in phone_book:
+        
         start_number = ""
         for number in phone_number:
-            start_number += number
+            start_number = start_number + number
 
             if start_number != phone_number and start_number in check_dict:
                 return False
--- Programmers/전화번호목록_0111.py
+++ Programmers/전화번호목록_0111.py
@@ -1,16 +1,20 @@
+
 def solution(phone_book):
     check_dict = {}
 
     for phone_number in phone_book:
         check_dict[phone_number] = True
+        
+        #check_dict = ["123":True,"456":True,"789":True]
 
-    for phone_number in phone_book:
+    for phone_number in phone_book: # phone_number "123"
         start_number = ""
-        for number in phone_number:
-            start_number += number
+        
+        for number in phone_number: # number 1, 2,  3 
+            start_number += number # "1" , "12", "123"
 
             if start_number != phone_number:
-                if check_dict.get(start_number) != None:
+                if check_dict.get(start_number) != None: 
                     return False
     else:
         return True
