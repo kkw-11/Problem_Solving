06a5982fe86b5380b415108592d5f57afef7a507
asus <kkw-11@naver.com>
2022-01-18 11:53:25 +0900

    220118  Programmers 베스트앨범 refactor

--- Programmers/베스트앨범_0117.py
+++ Programmers/베스트앨범_0117.py
@@ -8,10 +8,10 @@ def solution(genres, plays):
         else:
             best[genre][0] += plays[index]
             best[genre][1].append((plays[index], index))
-    best_genre_sort = sorted(best.values(), key=lambda x:-x[0])
+    best_genres_sort = sorted(best.values(), key=lambda x:-x[0])
 
-    for song in best_genre_sort:
-        rank_song = [i[1] for i in sorted(song[1],key = lambda x:(-x[0],x[1]))][:2]
+    for genre_play_info in best_genres_sort:
+        rank_song = [i[1] for i in sorted(genre_play_info[1],key = lambda x:(-x[0],x[1]))][:2]
         answer.extend(rank_song)
 
     return answer
