408086cb0c368947c75208e14b814194f7698315
asus <kkw-11@naver.com>
2022-01-21 23:46:25 +0900

    220121 Programmers 베스트앨범 refactoring

---
+++ Programmers/베스트앨범_0121_1.py
@@ -0,0 +1,15 @@
+def solution(genres, plays):
+    answer = []
+    best = {}
+    
+    for song_number, genre in enumerate(genres):
+        if not genre in best:
+            best[genre] = [plays[song_number], [(song_number,plays[song_number])]]
+        else:
+            best[genre][0] += plays[song_number]
+            best[genre][1].append((song_number,plays[song_number]))
+            
+    for plays_infos in sorted(best.values(),key=lambda x:-x[0]):
+        answer.extend([a[0] for a in sorted(plays_infos[1], key=lambda x:(-x[1],x[0]))[:2]])
+    
+    return answer
