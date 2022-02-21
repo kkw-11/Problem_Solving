e2bf20b539941df6f349e2a6245504ee948e2755
asus <kkw-11@naver.com>
2022-02-21 21:26:08 +0900

    220221 Programmers 기능개발

---
+++ Programmers/기능개발_0221.py
@@ -0,0 +1,24 @@
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    
+    remain_day = []
+    distribution = []
+    for index, progress in enumerate(progresses):
+        remain_day.append(math.ceil((100-progress)/speeds[index]))
+    
+    for day in remain_day:
+        if len(distribution) == 0:
+            distribution.append(day)  
+        elif len(distribution) != 0 and distribution[0] >= day:
+            distribution.append(day)  
+        elif len(distribution) != 0 and distribution[0] < day:
+            answer.append(len(distribution))
+            distribution = []
+            distribution.append(day)
+            
+    answer.append((len(distribution)))
+        
+    
+    return answer
