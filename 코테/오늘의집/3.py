d159c9087030cc41005db6ec1497a3b50336b29d
asus <kkw-11@naver.com>
2022-04-16 01:07:22 +0900

    220416 오늘의집 3

--- 코테/오늘의집/3.py
+++ 코테/오늘의집/3.py
@@ -5,7 +5,7 @@ def solution(tstring, variables):
     vari_dict = {}
 
     for variable in variables:
-        vari_dict[variable[0]] = [variable[1],False]
+        vari_dict[variable[0]] = [variable[1],False,None]
     
     for index, temp_word in enumerate(tlist):
         value_word = temp_word
@@ -14,20 +14,23 @@ def solution(tstring, variables):
             if value_word[1:-1] in vari_dict:
                 if vari_dict[value_word[1:-1]][1] == False:
                     while value_word[0] == '{' and value_word[-1] == '}':
-                        value_word = vari_dict[value_word[1:-1]][0]
-                        if value_word == temp_word:
+                        if value_word[1:-1] in vari_dict:
+                            value_word = vari_dict[value_word[1:-1]][0]
+                            if value_word == temp_word or (value_word[1:-1] in vari_dict and value_word == vari_dict[value_word[1:-1]][0]):
+                                break
+                        else:
                             break
-
+                        
 
         tlist[index] = value_word
         if temp_word[1:-1] in vari_dict and vari_dict[temp_word[1:-1]][1] == False:
-            vari_dict[temp_word[1:-1]][0] = value_word
+            vari_dict[temp_word[1:-1]][2] = value_word
             vari_dict[temp_word[1:-1]][1] = True
 
     answer = " ".join(tlist)
     return answer
 
 tstring = "this is {template} {template} is {state}"
-variables = [["template","string"], ["state","changed"]]
+variables = [["template","{state}"], ["state","{templates}"]]
 
 print(solution(tstring, variables))
