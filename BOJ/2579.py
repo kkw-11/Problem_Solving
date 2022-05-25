189352fee860fd7f66512795815f1add800e45b2
asus <kkw-11@naver.com>
2022-05-25 19:37:00 +0900

    220525 BOJ 11726

--- BOJ/2579.py
+++ BOJ/2579.py
@@ -14,4 +14,4 @@ dp[2] = stair[1]+stair[2]
 
 for i in range(3,n+1):
     dp[i] = max(stair[i]+stair[i-1]+dp[i-3],stair[i]+dp[i-2])
-print(dp[n])
+print(dp[n])g
--- 파이썬 알고리즘 문제풀이/DP_section8/네트워크선자르기_Top_Bottom_recur.py
+++ 파이썬 알고리즘 문제풀이/DP_section8/네트워크선자르기_Top_Bottom_recur.py
@@ -15,20 +15,20 @@ dy = [0]*(n+1)
 print(recur(n))
 
 
-# import sys
+import sys
 
-# def recur(n):
-#     if n == 1 or n == 2:
-#         dy[n] = n
-#         return dy[n]
-#     else:
-#         if dy[n] > 0:
-#             return dy[n]
-#         else:
-#             dy[n] = recur(n-1) + recur(n-2) 
-#             return dy[n]
+def recur(n):
+    if n == 1 or n == 2:
+        dy[n] = n
+        return dy[n]
+    else:
+        if dy[n] > 0:
+            return dy[n]
+        else:
+            dy[n] = recur(n-1) + recur(n-2) 
+            return dy[n]
 
-# n = int(input())
-# dy = [0]*(n+1)
+n = int(input())
+dy = [0]*(n+1)
 
-# print(recur(n))
+print(recur(n))
