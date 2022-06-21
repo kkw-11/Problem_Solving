48a53132eb792492f65741b6634ec5b42bc264a5
asus <kkw-11@naver.com>
2022-06-21 21:37:02 +0900

    220621 KKAOPAY 2.PY 변수명 수정

--- 코테/카카오페이/2.py
+++ 코테/카카오페이/2.py
@@ -1,18 +1,18 @@
-def solution(n,m,rectangle):
+def solution(n,m,rectangles):
     answer = []
     rectangle.sort()
     table = [[0]*n for _ in range(m)]
 
-    for rec in rectangle:
-        for _ in range(rec[1]):
+    for rectangle in rectangles:
+        for _ in range(rectangle[1]):
                       
             for row in range(m):
                 find_where = False
                 not_find =  False
                 for col in range(n):
                     if table[row][col] == 0:
-                        if col + rec[0] - 1 >=n or row+rec[0]-1>=m: continue
-                        for i in range(rec[0]):
+                        if col + rectangle[0] - 1 >= n or row + rectangle[0] - 1 >= m: continue #2차원 평면 범위 체크
+                        for i in range(rectangle[0]):
                             if table[row][col+i] != 0:
                                 not_find = True
                                 break
@@ -21,14 +21,14 @@ def solution(n,m,rectangle):
                         find_where = True
                         break
                 if find_where:
-                    answer.append([col,row,rec[0]])
-                    for i in range(rec[0]):
-                        for j in range(rec[0]):
-                            table[row+i][col+j] = rec[0]
+                    answer.append([col,row,rectangle[0]])
+                    for i in range(rectangle[0]):
+                        for j in range(rectangle[0]):
+                            table[row+i][col+j] = rectangle[0]
                     break
     return answer
 
 n = 7
 m = 8
-rectangle = [[2,2],[1,4],[3,2]]
-print(solution(n,m,rectangle))
+rectangles = [[2,2],[1,4],[3,2]]
+print(solution(n,m,rectangles))
