2b2f3a7b57bb346c6a226122065d67744a7115f0
asus <kkw-11@naver.com>
2022-07-25 17:38:08 +0900

    220725 Programmers 부족한 금액 계산하기

--- KAKAO/크레인인형뽑기게임.java
+++ KAKAO/크레인인형뽑기게임.java
@@ -10,14 +10,14 @@ class Solution {
             for(int j = 0; j < board.length; ++j){
                 if (board[j][moves[i]-1] == 0) continue;
                 else{
-                    basket.add(board[j][moves[i]-1]);
+                    basket.push(board[j][moves[i]-1]);
                     board[j][moves[i]-1] = 0;
                
                     if(basket.size() > 1){
                         if(basket.get(basket.size()-1) == basket.get(basket.size()-2)){
                             answer += 2;
-                            basket.remove(basket.size()-1);
-                            basket.remove(basket.size()-1);
+                            basket.pop();
+                            basket.pop();
                         }
                     }
                     break;
---
+++ Programmers/부족한금액계산하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long total = 0;
+        
+        for(int i = 1; i <= count; ++i){
+            total += price*i;
+        }
+        
+        if(money > total) 
+            return 0;
+        else 
+            return total - money;
+         
+    }
+}
