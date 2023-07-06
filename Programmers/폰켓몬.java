b2d1562300f86344bd85a652fae71a8e5f24907d
Kiwon <76929823+kkw-11@users.noreply.github.com>
2023-07-06 15:30:54 +0900

    Create 폰켓몬.java

---
+++ Programmers/폰켓몬.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+  int answer = 0;
+
+        int halfNumsLen = nums.length/2;
+
+        Set<Integer> ponKetMonSet = new HashSet<>();
+        for (Integer ponKetMonNum : nums) {
+            ponKetMonSet.add(ponKetMonNum);
+        }
+
+        if (ponKetMonSet.size() > halfNumsLen) {
+            answer = halfNumsLen;
+        } else {
+            answer = ponKetMonSet.size();
+        }
+
+        return answer;
+    }
+}
