eacce3c8303c94dc4e233ff8f033a5fd9522b33b
kiwon <kkw-11@naver.com>
2025-06-18 17:35:30 +0900

    BOJ 1152

---
+++ JavaProblemSolving/BOJ/src/No1152.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class No1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().strip();
+        if (input.isBlank()) {
+            System.out.println(0);
+        }else {
+            String[] splitInput = input.split("\\s+");
+
+            System.out.println(splitInput.length);
+        }
+        scanner.close();
+    }
+}
