cdfa453522ac4ea750457e507e07c08ee60df802
kiwon <kkw-11@naver.com>
2025-06-19 08:53:12 +0900

    feat:BOJ 2734

---
+++ JavaProblemSolving/BOJ/src/No2734.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class No2734 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String inpuString = sc.next();
+
+        System.out.println(inpuString.length());
+    }
+}
