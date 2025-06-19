b085057c5ce9e063cd5bea5dbc212d3e5dbac717
kiwon <kkw-11@naver.com>
2025-06-19 09:44:07 +0900

    feat:BOJ 11654

---
+++ JavaProblemSolving/BOJ/src/No11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class No11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        System.out.println(input.charAt(0)+0);
+
+        sc.close();
+    }
+}
